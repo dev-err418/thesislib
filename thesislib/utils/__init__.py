c079c77a1f37d7748b5af0e4ef14555b95d0b334
Obinna Agba <obinna@prowork.me>
2020-06-20 00:25:35 +0200

    having a packaging for python 101

---
+++ thesislib/utils/__init__.py
@@ -0,0 +1,6 @@
+from . import knifeutils, pathutils, statutils, stringutils
+from . import ml
+from . import imput
+
+
+__all__ = ["knifeutils", "pathutils", "statutils", "stringutils", "ml", "imput"]
---
+++ thesislib/utils/imput/__init__.py
@@ -0,0 +1,2 @@
+from . import imput
+from . import utils
