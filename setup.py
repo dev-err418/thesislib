7befa5031c9258d12863ab160c17291edc2a076b
Obinna Agba <obinna@prowork.me>
2020-06-20 00:52:33 +0200

    updated setup

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
    name='thesislib',
@@ -6,6 +6,6 @@ setup(
    description='Collection of functions/utilities for my thesis',
    author='O.S. Agba',
    author_email='',
-   packages=['thesislib'],
+   packages=find_packages(),
    install_requires=['pandas', 'numpy', 'matplotlib', 'requests', 'tabulate', 'scikit-learn', 'python-dateutil'],
 )
